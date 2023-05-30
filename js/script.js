ffbd70d1568837812902a0e4bbd5ecbc7bf17391
Alida Gorman <128799448+atgorman1@users.noreply.github.com>
2023-05-30 09:44:17 -0400

    Added API to fetch GH repos and display them on the page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 // profile information overview that will appear at the top of the page
 const overview = document.querySelector(".overview");
 const username = "atgorman1";
+// unordered list that lists all of the repos
+const repoUl = document.querySelector(".repo-list");
 
 // async function that will pull the GH profile info
 const getProfile = async function () {
@@ -24,4 +26,24 @@ const displayProfileData = async function (profile) {
       <p><strong>Number of public repos:</strong> ${profile.public_repos}</p>
     </div>`;
   overview.append(div);
+  getRepos();
+};
+
+// async function that will pul the GH repos
+const getRepos = async function () {
+  const res = await fetch(
+    `https://api.github.com/users/${username}/repos?sort=updated&direction=asc&per_page=100`
+  );
+  const repos = await res.json();
+  console.log(repos);
+  displayRepoInfo(repos);
+};
+
+const displayRepoInfo = function (repos) {
+  for (const repo of repos) {
+    const repoItem = document.createElement("li");
+    repoItem.classList.add("repo");
+    repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+    repoUl.append(repoItem);
+  }
 };
