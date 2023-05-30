66261fb81139ddb054292ca8c0a2e7fd3d5a99cd
Alida Gorman <128799448+atgorman1@users.noreply.github.com>
2023-05-30 13:13:59 -0400

    Added repo info from GH API when repo name is clicked

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@ const overview = document.querySelector(".overview");
 const username = "atgorman1";
 // unordered list that lists all of the repos
 const repoUl = document.querySelector(".repo-list");
+// repo class where all the repo info appears
+const repoInfoSpace = document.querySelector(".repos");
+// repo-data class where indicidual repo data appears
+const repoData = document.querySelector(".repo-data");
 
 // async function that will pull the GH profile info
 const getProfile = async function () {
@@ -32,18 +36,57 @@ const displayProfileData = async function (profile) {
 // async function that will pul the GH repos
 const getRepos = async function () {
   const res = await fetch(
-    `https://api.github.com/users/${username}/repos?sort=updated&direction=asc&per_page=100`
+    `https://api.github.com/users/${username}/repos?sort=updated&direction=desc&per_page=100`
   );
   const repos = await res.json();
   console.log(repos);
-  displayRepoInfo(repos);
+  displayRepoName(repos);
 };
 
-const displayRepoInfo = function (repos) {
+const displayRepoName = function (repos) {
   for (const repo of repos) {
     const repoItem = document.createElement("li");
     repoItem.classList.add("repo");
     repoItem.innerHTML = `<h3>${repo.name}</h3>`;
     repoUl.append(repoItem);
   }
+};
+
+repoUl.addEventListener("click", function (e) {
+  if (e.target.matches("h3")) {
+    const repoName = e.target.innerText;
+    getRepoDetails(repoName);
+  }
+});
+
+const getRepoDetails = async function (repoName) {
+  const res = await fetch(
+    `https://api.github.com/repos/${username}/${repoName}`
+  );
+  const repoInfo = await res.json();
+  console.log(repoInfo);
+  const fetchLanguages = await fetch(repoInfo.languages_url);
+  const languageData = await fetchLanguages.json();
+  console.log(languageData);
+  const languages = [];
+  for (language in languageData) {
+    languages.push(language);
+  }
+  console.log(languages);
+  displayRepoDetails(repoInfo, languages);
+};
+
+const displayRepoDetails = function (repoInfo, languages) {
+  repoData.innerHTML = "";
+  const repoInfoDiv = document.createElement("div");
+  repoInfoDiv.innerHTML = `<h3>Name: ${repoInfo.name}</h3>
+    <p>Description: ${repoInfo.description}</p>
+    <p>Default Branch: ${repoInfo.defualt_branch}</p>
+    <p>Languages: ${languages.join(", ")}</p>
+    <a class="visit" href="${
+      repoInfo.html_url
+    }" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>`;
+  repoData.append(repoInfoDiv);
+  repoInfoSpace.classList.add("hide");
+  repoData.classList.remove("hide");
 };
